26c8a2b690adb66ed4fed71875d708fc802d58c0
clach04 <clach04@gmail.com>
2025-06-17 14:47:36 -0700

    Fixes #9 - spelling mistake in help

--- export_stars/export_stars.py
+++ export_stars/export_stars.py
@@ -29,7 +29,7 @@ def config_retry(backoff_factor=1.0, total=8):
 
 
 def parse_args():
-    parser = ArgumentParser(description="export a GitHub user's starred repositorys to CSV")
+    parser = ArgumentParser(description="export a GitHub user's starred repositories to CSV")
     parser.add_argument("--user", dest='user')
     parser.add_argument("--github-token", dest='token')
     return parser.parse_args()
